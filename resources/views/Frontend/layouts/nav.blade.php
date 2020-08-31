f491e55c882eb0a43c4ee95fd2dbe0ffab6145ad
y wyne ko <ywyneko@gmail.com>
2020-08-31 23:28:05 +0630

    Fixed Files

--- resources/views/Frontend/layouts/nav.blade.php
+++ resources/views/Frontend/layouts/nav.blade.php
@@ -10,11 +10,10 @@
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li class="active"><a href="#index.html">Home</a></li>
+          <li class="active"><a href="#hero">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#services">Services</a></li>
           <li><a href="#team">Team</a></li>
-          <li><a href="#pricing">Pricing</a></li>
           <li><a href="#contact">Contact</a></li>
           <li class="get-started"><a href="{{ route('login') }}">Login</a></li>
           <li class="get-started"><a href="{{ route('register') }}">Register</a></li>
