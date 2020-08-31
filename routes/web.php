46c110fde302cd8a9c7e70534812a5abf7e86d76
y wyne ko <ywyneko@gmail.com>
2020-08-31 18:27:52 +0630

    Fixed

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,11 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// <<<<<<< HEAD
+
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/', 'PageController@index')->name('page');
-
-// =======
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/home', 'HomeController@index')->name('home');
     
 });
-//  44bcd933c665f51ce79e773002fdd922ed20c9b9
+
