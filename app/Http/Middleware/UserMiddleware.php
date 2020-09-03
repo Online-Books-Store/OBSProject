699d6e68a18c5853bec8114068faad63cd010919
zeyartun <zeyarking125@gmail.com>
2020-09-03 11:14:44 +0630

    add userMiddleware

---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\User;
+
+class UserMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(auth()->user()->status == 'admin'){
+
+            return $next($request);
+        }else{
+            return redirect('/');
+        }
+    }
+}
