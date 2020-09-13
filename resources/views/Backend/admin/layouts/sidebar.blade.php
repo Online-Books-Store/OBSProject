09f29d777b2dfd655fadb7bc2496b17f82f9d3e3
aungthawtar <aungthawtar@example.com>
2020-09-13 19:18:31 +0630

    update user image

--- resources/views/Backend/admin/layouts/sidebar.blade.php
+++ resources/views/Backend/admin/layouts/sidebar.blade.php
@@ -10,7 +10,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="{{isset(Auth::user()->image) ? asset("uploads/".Auth::user()->image) : 'http://lorempixel.com/200/200/people/9/'}} " class="avatar img-circle" style="width:40px; height:40px;" alt="User Image">
         </div>
 
         <div class="info">
